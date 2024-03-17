586c596b5ccb0ceae9a52044020535ecf3abf695
Nash Chaney <nchaney0508@gmail.com>
2024-03-17 15:07:31 -0500

    Completed GA 6 - No Styling

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+    const instructions = "Click on the buttons to see the satellites in that orbit type";
+  
+    return `
+    Orbit Report -
+    ${instructions}
+    `;
+  }
+  
+  export default Banner;
